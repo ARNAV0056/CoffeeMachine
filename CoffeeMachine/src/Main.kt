3133f54f230d940e45e91aee4399fbdb374bb8f8
Arnav Vashista <91889179+ARNAV0056@users.noreply.github.com>
2024-06-01 22:39:04 +0530

    Main.kt

--- CoffeeMachine/src/Main.kt
+++ CoffeeMachine/src/Main.kt
@@ -1,17 +1,29 @@
-package arnav.CoffeMachine
+package arnav.CoffeeMachine
 fun main(){
-makeCoffee()//called a function makecoffee
+    println("What is your name?")
+    val Name= readln()
+    println("how much sugar do you want ")
+    val sugarCount:Int= readln().toInt()
+
+    makeCoffee(Name,sugarCount)//called a function makeCoffee
 
 }
-fun makeCoffee(){
-    println("what is your name?")
-    var askName= readln()
+fun makeCoffee(Name:String,sugarCount :Int){
+    println("$Name took $sugarCount cube of Sugar in their coffee")
     println("Which coffee you want")
     val button= readln()
     println("What kind of beans you want?")
     val beans= readln()
     println("What kind of milk you want?")
     var milk= readln()
+    when(button) {
+        "Americano" -> println("OK! now  your Americano is brewed")
+        "Cappuccino" -> println("OK! now your cappuccino is brewed ")
+        else->{
+            println("Sorry we do not have that right now")
+        }
+
+    }
     when(beans){
         "Roasted"-> println("OK! Roasted Beans are selected")
         "Classic"-> println("OK! Classic Beans are selected")
@@ -21,9 +33,10 @@ fun makeCoffee(){
         "Soy"-> println("OK! Soy milk has been selected")
         "Normal"-> println("OK! Normal milk has been selected")
     }
-    when(button) {
-        "Americano" -> println("OK! now  your Americano is brewed")
-        "Cappuccino" -> println("OK! now your cappuccino is brewed ")
-
+    if(sugarCount==0){
+        println( println("$button with $beans beans and $milk milk for $Name with 0 sugar is now ready!!!"))
+    }
+    else{
+    println("$button with $beans beans and $milk milk for $Name with $sugarCount cube of sugar is now ready!!!")
     }}
 
